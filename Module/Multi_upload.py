f7e52493fb2060370d2606f069154e00361a6e68
Officialahmed <Ahmed.riyami1818@gmail.com>
2023-06-14 14:30:23 +0400

    Added Mask maker methods

--- Module/Multi_upload.py
+++ Module/Multi_upload.py
@@ -2,21 +2,27 @@
     Class to handle uploading of multiple baked icons
 """
 
+import os, json
 from environment import Common
 import Common.Uploader as Uploader
-
 
 class Main(Common):
     
     def __init__(self) -> None:
         super().__init__()
-        self.uploader = Uploader()
+        self.uploader = Uploader.Main()
+        self.icons = None
 
 
     def upload_baked_icons(self, state_widget, group_path):
+
         state_widget.setText("Uploading...")
-        print(group_path)
-    
-        
-    def get_icon_group(self):
-        self.GroupName
+
+        self.icons = json.load(open(group_path))
+        baked_icons = os.listdir(self.baked_path)
+
+        for icon in self.icons:
+
+            if f"{icon}.png" in baked_icons:
+
+                self.uploader.update_local_icon(f"{self.baked_path}{icon}.png", icon)
