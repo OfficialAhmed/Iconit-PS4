172c0cfce9142400556de120a164073f01b8e116
Officialahmed <Ahmed.riyami1818@gmail.com>
2023-09-02 19:42:21 +0400

    ADDED: PS5 constant environment

--- Module/Constant/Constants.py
+++ Module/Constant/Constants.py
@@ -1,16 +1,17 @@
-""" Read-only class """
+""" Read-only classes """
 
-class Constants:
-    # ___________   Private attrs   _______________#
+class PS4:
+    # ___________   Private PS4 constant attrs   _______________#
 
-    __PS4_SYS_SCE = "sce_sys"
-    __PS4_ICON_SIZE = (512, 512)
-    __PS4_PIC_SIZE = (1920, 1080)
-    __PS4_4k_ICON_SIZE = (660, 660)
-    __PS4_INT_ICONS = "user/appmeta/"
-    __PS4_SYS_ICONS = "system_ex/app/"
-    __PS4_EXT_ICONS = "user/appmeta/external/"
-    __PS4_PRONOUNCIATION_FILE = "pronunciation.xml"
+    __SYS_SCE = "sce_sys"
+    __ICON_SIZE = (512, 512)
+    __PIC_SIZE = (1920, 1080)
+    __4k_ICON_SIZE = (660, 660)
+    __USER_DATA = "user/data/" 
+    __INT_ICONS = "user/appmeta/"
+    __SYS_ICONS = "system_ex/app/"
+    __EXT_ICONS = "user/appmeta/external/"
+    __PRONOUNCIATION_FILE = "pronunciation.xml"
 
     __ICONS_BACKUP_NAME = "Backup"
     __ICON_SUPPORTED_FORMATS = "Image (*.png *.jpg *.jpeg)"
@@ -30,34 +31,43 @@ class Constants:
         raise AttributeError(f"READ-ONLY: Class 'Constant' allow getters only. Occured while trying to set [{__name}]")
         
     def get_color(self, str_color:str) -> str:
-        return Constants.__HASH_THEME_COLOR[str_color]
+        return PS4.__HASH_THEME_COLOR[str_color]
 
     def get_ps4_pic_size(self) -> tuple:
-        return Constants.__PS4_PIC_SIZE
+        return PS4.__PIC_SIZE
 
     def get_ps4_icon_size(self) -> tuple:
-        return Constants.__PS4_ICON_SIZE
+        return PS4.__ICON_SIZE
 
     def get_ps4_4k_icon_size(self) -> tuple:
-        return Constants.__PS4_4k_ICON_SIZE
+        return PS4.__4k_ICON_SIZE
 
     def get_sce_folder_name(self) -> str:
-        return Constants.__PS4_SYS_SCE
+        return PS4.__SYS_SCE
 
     def get_system_icons_path(self) -> str:
-        return Constants.__PS4_SYS_ICONS
+        return PS4.__SYS_ICONS
 
     def get_backup_folder_name(self) -> str:
-        return Constants.__ICONS_BACKUP_NAME
+        return PS4.__ICONS_BACKUP_NAME
 
     def get_internal_icons_path(self) -> str:
-        return Constants.__PS4_INT_ICONS
+        return PS4.__INT_ICONS
 
     def get_external_icons_path(self) -> str:
-        return Constants.__PS4_EXT_ICONS
+        return PS4.__EXT_ICONS
 
     def get_icon_supported_format(self) -> str:
-        return Constants.__ICON_SUPPORTED_FORMATS
+        return PS4.__ICON_SUPPORTED_FORMATS
 
     def get_pronunciation_file_path(self) -> str:
-        return Constants.__PS4_PRONOUNCIATION_FILE
+        return PS4.__PRONOUNCIATION_FILE
+    
+    def get_user_data_path(self) -> str:
+        return PS4.__USER_DATA
+
+
+class PS5:
+    # ___________   Private PS5 constant attrs   _______________#
+
+    pass
